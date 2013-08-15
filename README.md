f5c412eb49a7c8f420a1d923a0e0ec1f39ff1b48
John Beynon <john@beynon.org.uk>
2013-08-15 13:11:00 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+Heroku Rails New
+================
+
+Heroku CLI plugin that generates a Heroku ready Rails application
+
+    heroku plugins:install https://github.com/johnbeynon/heroku-rails-new.git
+
+Running
+
+    heroku new:rails [APP_PATH]
+
+What it does
+
+    Generates a new Rails application (depends on installed version) and then
+    runs my heroku-rails-template
+    (https://github.com/johnbeynon/heroku-rails-template) against the new
+    application.
+
---
+++ init.rb
@@ -0,0 +1,19 @@
+# Create new applicaions tailored for deployment to Heroku
+# 
+class Heroku::Command::New < Heroku::Command::Base
+  # new:rails [APP_PATH]
+  #
+  # Creates a new Rails app in APP_PATH and runs heroku-rails-template against
+  #
+  # Examples:
+  #
+  # $ heroku new:rails exampleapp
+  #
+  def rails
+    unless name = shift_argument
+      error("Usage: heroku new:rails [APP_PATH]")
+    end
+    
+    system "rails new #{name} -m https://raw.github.com/johnbeynon/heroku-rails-template/master/generator.rb"
+  end
+end
